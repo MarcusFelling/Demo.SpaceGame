6ff9aa6a687e2f6d44f8ebfa64e693c4889cefa1
Marcus Felling <MarcusFelling@microsoft.com>
2021-02-05 11:43:26 -0600

    add required rm provider

--- Tailspin.SpaceGame.Terraform/webapp.tf
+++ Tailspin.SpaceGame.Terraform/webapp.tf
@@ -7,6 +7,12 @@ terraform {
       name = "DemoSpaceGame"
     }
   }
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=2.46.0"
+    }
+  }
 }
 
 provider "azurerm" {
