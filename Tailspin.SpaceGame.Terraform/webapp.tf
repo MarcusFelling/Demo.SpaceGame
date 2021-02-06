f3c247e02b7dc646a1e4d4513da7b59fc90debd4
Marcus Felling <MarcusFelling@microsoft.com>
2021-02-06 11:39:50 -0600

    switch TF Cloud to local

--- Tailspin.SpaceGame.Terraform/webapp.tf
+++ Tailspin.SpaceGame.Terraform/webapp.tf
@@ -1,12 +1,3 @@
-variable "subscription_id" {
-}
-variable "client_id" {
-}
-variable "client_secret" {
-}
-variable "tenant_id" {
-}
-
 # Use TF Cloud for Backend
 terraform {
   required_providers {
@@ -28,10 +19,6 @@ terraform {
 # Azure Provider
 provider "azurerm" {
   features {}
-  subscription_id = var.ARM_SUBSCRIPTION_ID
-  client_id       = var.ARM_CLIENT_ID
-  client_secret   = var.ARM_CLIENT_SECRET
-  tenant_id       = var.ARM_TENANT_ID
 }
 
 # Variables
